7b7d1fd509a91d17bbd8023d9f4878bcc9fa0cfc
Gabriela Webb <gabw813@gmail.com>
2022-11-15 23:23:30 -0500

    updates react boilerplate code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>SHYBYE!</h1>
       </header>
+      <body>
+        <p>
+          This is the start of <i>SHYBYE!</i>, an app hacked during GDI
+          Hackathon '22! ✨
+        </p>
+      </body>
+      <footer>&copy; 2022 team shygirls &hearts;</footer>
     </div>
   );
 }
