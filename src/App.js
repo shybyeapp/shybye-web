accf4d2ecdef200c9aacf776c5c58d4b5e6e7a4b
Gabriela Webb <gabw813@gmail.com>
2022-11-16 22:43:32 -0500

    adds axios & post/get user functions; not tested yet

--- src/App.js
+++ src/App.js
@@ -1,16 +1,47 @@
 import "./App.css";
 import React from "react";
+import axios from "axios";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import AboutTeam from "./components/AboutTeam";
 import ResearchPage from "./components/ResearchPage";
-// import { useState } from "react";
+import { useState } from "react";
 // import UserPage from "./components/UserPage";
 
 export default function App() {
-  // const navigate = useNavigate();
+  const BACKENDURL = "";
 
   // current user state keeps track of the user that is currently logged in
-  // const [currentUser, setCurrentUser] = useState([]);
+  const [currentUser, setCurrentUser] = useState([]);
+
+  // async call to db: POST user
+  const addNewUser = (userInfo) => {
+    console.log("adding user");
+    axios
+      .post(`${BACKENDURL}/users`)
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  // async call to db: GET user
+  const getReturningUser = (userInfo) => {
+    console.log("adding user");
+    axios
+      .post(`${BACKENDURL}/users`)
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  // async call to db: GET user's challenges
+
+  // async call to db: GET user's score
 
   return (
     <section className="App">
