27ac03a6475ebea10e50fc6ed686fb3c7fe1248f
Gabriela Webb <gabw813@gmail.com>
2022-11-18 14:16:40 -0500

    add button & fix layout of home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,14 @@
-import { Grid } from "@mui/material";
+import { Button, Grid } from "@mui/material";
 
 const Home = () => {
   return (
     <section>
-      <Grid container spacing={2} justifyContent="center">
+      <Grid container spacing={2} alignItems="center" justifyContent="center">
         <Grid item xs={6}>
           Complete challenges, collect rewards, reduce social anxiety!
+          <br></br>
+          <br></br>
+          <Button variant="contained">Get Started</Button>
         </Grid>
         <Grid item xs={6}>
           <img
