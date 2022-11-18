998e801fa2d41c063828225e3200bf9630c6ea8c
Valorieb <valoriebroderick@gmail.com>
2022-11-18 13:00:12 -0600

    experimenting/troubleshooting

--- src/App.js
+++ src/App.js
@@ -24,7 +24,84 @@ export default App;
 
 {
   
-  /* FOR YOUR COPY AND PASTE REFERENCE, HERE ARE THE IMAGE URLS FOR THE BADGES: 
+  
+      }
+
+      export const _App = ({ pageProps, Component }) => {
+
+        return (
+          <>
+            <style jsx global>{`
+              html,
+              body {
+                padding: 0;
+                margin: 0;
+                font-family: josefin sans;
+              }
+      
+              body {
+                background-color: #fff;
+              }
+      
+              a {
+                color: inherit;
+                text-decoration: none;
+              }
+      
+              * {
+                box-sizing: border-box;
+              }
+      
+              header {
+                height: 100px;
+                position: sticky;
+                top: 0;
+                margin-top: 32px;
+                background-color: #fff
+              }
+      
+              nav {
+                max-width: 760px;
+                padding: 32px;
+                display: flex;
+                justify-content: flex-end;
+                align-items: center;
+                margin: 0 auto;
+              }
+      
+              button {
+                border: 0;
+                border-radius: 4px;
+                height: 40px;
+                min-width: 40px;
+                padding: 0 8px;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                background-color: #e2e8f0;
+                cursor: pointer;
+                color: #fff;
+                margin-left: 16px;
+              }
+      
+              button:hover, button:focus, button:active {
+                background-color: var(--button-bg-hover);
+                outline: none;
+              }
+            `}</style>
+            <header>
+              <nav>
+                <button>Toggle</button>
+              </nav>
+            </header>
+            <Component {...pageProps} />
+          </>
+        );
+      };
+      
+     
+
+      /* FOR YOUR COPY AND PASTE REFERENCE, HERE ARE THE IMAGE URLS FOR THE BADGES: 
   
   <img class= "badges" src="/Assets/Images/Badges/balloon.png" alt="balloon"></img>
         <img class= "badges" src="/Assets/Images/Badges/dating-app.png" alt="speech bubbles with hearts"></img>
@@ -41,4 +118,4 @@ export default App;
         <img class= "badges" src="/Assets/Images/Badges/spoon-and-fork.png" alt="spoon and fork"></img>
         <img class= "badges" src="/Assets/Images/Badges/thumbs-down.png" alt="thumbs down"></img>
         <img class= "badges" src="/Assets/Images/Badges/smiley1.jpg" alt="smiley face"></img>
-        <img class= "badges" src="/Assets/Images/Badges/romantic-date.png" alt="people in love"></img> */}
+        <img class= "badges" src="/Assets/Images/Badges/romantic-date.png" alt="people in love"></img> */
--- src/index.js
+++ src/index.js
@@ -9,7 +9,31 @@ root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
+
+  
 );
+export default function Index() {
+  return <>
+    <style jsx>{`
+      .wrapper {
+        max-width: 760px;
+        padding: 0 32px;
+        margin: 0 auto;
+      }
+    `}</style>
+    <main className="page">
+      <div className="wrapper">
+        <h1 className="intro">Hello World!</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci
+          animi consectetur delectus dolore eligendi id illo impedit iusto,
+          laudantium nam nisi nulla quas, qui quisquam voluptatum? Illo nostrum
+          odit optio.
+        </p>
+      </div>
+
+    </main>
+  </>;
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
