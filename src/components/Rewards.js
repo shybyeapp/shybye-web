20ebde47742784299108d7750836fb446ab9b172
Cristinejohnson <cristine.i.johnson@gmail.com>
2022-11-18 16:08:28 -0700

    Made some edits to Rewards, added image

--- src/components/Rewards.js
+++ src/components/Rewards.js
@@ -1,11 +1,18 @@
 const Rewards = () => {
   return (
-    <section>
-      <img src="Images/placeholder_rectangle.png"></img>
+    <section class="rewardsContainer">
+      <img
+        src={require("../Images/Badges/balloon.png")}
+        alt="Congratulatory image"
+        class="congratsImg"
+      ></img>
       <h1>Congrats! You earned your first achievement:</h1>
       <p>Completing your first challenge</p>
       <p>Here's your medal:</p>
-      <img src="Images/placeholder_circle.png"></img>
+      <img
+        src="../Images/Badges/smiley1.jpg"
+        alt="Badge image"
+      ></img>
     </section>
   );
 };
