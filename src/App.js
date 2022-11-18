4f5e8d89f61003c2109483278cee62daa34ad868
Kandance Ferguson <kandance.ferguson@gmail.com>
2022-11-18 11:37:21 -0500

    added temp route for user page

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
-import "./App.css";
-import React from "react";
-import axios from "axios";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import Grid from "@mui/material/Grid";
-import Button from "@mui/material/Button";
-import Stack from "@mui/material/Stack";
-import Header from "./components/Header";
-import AboutTeam from "./components/AboutTeam";
-import Home from "./components/Home";
-import ResearchPage from "./components/ResearchPage";
-import { useState } from "react";
-import StickyFooter from "./components/Footer";
-// import UserPage from "./components/UserPage";
+import './App.css';
+import React from 'react';
+import axios from 'axios';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Grid from '@mui/material/Grid';
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
+import Header from './components/Header';
+import AboutTeam from './components/AboutTeam';
+import Home from './components/Home';
+import ResearchPage from './components/ResearchPage';
+import { useState } from 'react';
+import StickyFooter from './components/Footer';
+import UserPage from './components/UserPage';
 
 export default function App() {
-  const BACKENDURL = "";
+  const BACKENDURL = '';
 
   // current user state keeps track of the user that is currently logged in
   const [currentUser, setCurrentUser] = useState([]);
 
   // async call to db: POST user
   const addNewUser = (userInfo) => {
-    console.log("adding user");
+    console.log('adding user');
     axios
       .post(`${BACKENDURL}/users`)
       .then((res) => {
@@ -34,7 +34,7 @@ export default function App() {
 
   // async call to db: GET user
   const getReturningUser = (userInfo) => {
-    console.log("adding user");
+    console.log('adding user');
     axios
       .get(`${BACKENDURL}/users`)
       .then((res) => {
@@ -50,33 +50,34 @@ export default function App() {
   // async call to db: GET user's score
 
   return (
-    <section className="App">
+    <section className='App'>
       <Header></Header>
 
       <Router>
-        <div className="navbar">
+        <div className='navbar'>
           <nav>
-            <Stack spacing={2} direction="row" justifyContent="center">
-              <Button variant="text">
-                <Link to="/">Home </Link>
+            <Stack spacing={2} direction='row' justifyContent='center'>
+              <Button variant='text'>
+                <Link to='/'>Home </Link>
               </Button>
-              <Button variant="text">
-                <Link to="/about"> About </Link>
+              <Button variant='text'>
+                <Link to='/about'> About </Link>
               </Button>
-              <Button variant="text">
-                <Link to="/research">Research </Link>
+              <Button variant='text'>
+                <Link to='/research'>Research </Link>
               </Button>
             </Stack>
           </nav>
 
           <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/research" element={<ResearchPage />} />
-            <Route path="/about" element={<AboutTeam />} />
+            <Route path='/' element={<Home />} />
+            <Route path='/research' element={<ResearchPage />} />
+            <Route path='/about' element={<AboutTeam />} />
+            <Route path='/user' element={<UserPage />} />
           </Routes>
         </div>
       </Router>
-      <Grid container spacing={2} justifyContent="center">
+      <Grid container spacing={2} justifyContent='center'>
         <Grid item xs={12}></Grid>
       </Grid>
       <StickyFooter />
