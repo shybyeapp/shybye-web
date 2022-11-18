03e8062b7bf4a6721e135540c3fbff1492e2f241
Gabriela Webb <gabw813@gmail.com>
2022-11-18 14:36:05 -0500

    updated grid size

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,13 +4,13 @@ const Home = () => {
   return (
     <section>
       <Grid container spacing={2} alignItems="center" justifyContent="center">
-        <Grid item xs={6}>
+        <Grid item xs={12} md={6}>
           Complete challenges, collect rewards, reduce social anxiety!
           <br></br>
           <br></br>
           <Button variant="contained">Get Started</Button>
         </Grid>
-        <Grid item xs={6}>
+        <Grid item xs={12} md={6}>
           <img
             src={require("../Images/Logos/ShyBye.png")}
             alt="ShyBye logo"
