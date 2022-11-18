e0483aa05b011e994a52bf7a64c4a33a8a2533cd
Cristinejohnson <cristine.i.johnson@gmail.com>
2022-11-18 12:50:03 -0700

    Rewards page added, HTML and placeholder images

---
+++ src/components/Rewards.js
@@ -0,0 +1,13 @@
+const Rewards = () => {
+  return (
+    <section>
+      <img src="Images/placeholder_rectangle.png"></img>
+      <h1>Congrats! You earned your first achievement:</h1>
+      <p>Completing your first challenge</p>
+      <p>Here's your medal:</p>
+      <img src="Images/placeholder_circle.png"></img>
+    </section>
+  );
+};
+
+export default Rewards;
