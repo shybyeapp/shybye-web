ef51b869d2f2ae794731869cf62b08659f8c32a8
Gabriela Webb <gabw813@gmail.com>
2022-11-17 22:31:19 -0500

    resolving merge conflict

--- src/App.js
+++ src/App.js
@@ -50,7 +50,6 @@ export default function App() {
   // async call to db: GET user's score
 
   return (
-<<<<<<< HEAD
     <section className="App">
       <Header></Header>
 
@@ -84,45 +83,21 @@ export default function App() {
     </section>
   );
 }
-=======
-    <div className="App">
-      <header className="App-header">
-        <h1>SHYBYE!</h1>
-      </header>
-      <body>
-        <p>
-          This is the start of <i>SHYBYE!</i>, an app hacked during GDI
-          Hackathon '22! ✨
-        </p>
-        
-      </body>
-      <footer>&copy; 2022 team shygirls &hearts;</footer>
-    </div>
 
-    
-  );
-}
-
-export default App;
-
-{
-  
-  /* FOR YOUR COPY AND PASTE REFERENCE, HERE ARE THE IMAGE URLS FOR THE BADGES: 
-  
-  <img class= "badges" src="/Assets/Images/Badges/balloon.png" alt="balloon"></img>
-        <img class= "badges" src="/Assets/Images/Badges/dating-app.png" alt="speech bubbles with hearts"></img>
-        <img class= "badges" src="/Assets/Images/Badges/direction.png" alt="direction signs"></img>
-        <img class= "badges" src="/Assets/Images/Badges/eye.png" alt="eye"></img>
-        <img class= "badges" src="/Assets/Images/Badges/fist-bump.png" alt="fist-bump"></img>
-        <img class= "badges" src="/Assets/Images/Badges/five.png" alt="number five"></img>
-        <img class= "badges" src="/Assets/Images/Badges/hello.png" alt="hello"></img>
-        <img class= "badges" src="/Assets/Images/Badges/in-love.png" alt="smiley with heart eyes"></img>
-        <img class= "badges" src="/Assets/Images/Badges/old-man.png" alt="old man"></img>
-        <img class= "badges" src="/Assets/Images/Badges/question.jpg" alt="question mark"></img>
-        <img class= "badges" src="/Assets/Images/Badges/quoteBubbles.jpg" alt="quote bubbles"></img>
-        <img class= "badges" src="/Assets/Images/Badges/speech.png" alt="person giving speech"></img>
-        <img class= "badges" src="/Assets/Images/Badges/spoon-and-fork.png" alt="spoon and fork"></img>
-        <img class= "badges" src="/Assets/Images/Badges/thumbs-down.png" alt="thumbs down"></img>
-        <img class= "badges" src="/Assets/Images/Badges/smiley1.jpg" alt="smiley face"></img>
-        <img class= "badges" src="/Assets/Images/Badges/romantic-date.png" alt="people in love"></img> */}
->>>>>>> 816930895897d54d89d149cda8059175c715f2b3
+// FOR YOUR COPY AND PASTE REFERENCE, HERE ARE THE IMAGE URLS FOR THE BADGES:
+//   <img class= "badges" src="/Assets/Images/Badges/balloon.png" alt="balloon"></img>
+//         <img class= "badges" src="/Assets/Images/Badges/dating-app.png" alt="speech bubbles with hearts"></img>
+//         <img class= "badges" src="/Assets/Images/Badges/direction.png" alt="direction signs"></img>
+//         <img class= "badges" src="/Assets/Images/Badges/eye.png" alt="eye"></img>
+//         <img class= "badges" src="/Assets/Images/Badges/fist-bump.png" alt="fist-bump"></img>
+//         <img class= "badges" src="/Assets/Images/Badges/five.png" alt="number five"></img>
+//         <img class= "badges" src="/Assets/Images/Badges/hello.png" alt="hello"></img>
+//         <img class= "badges" src="/Assets/Images/Badges/in-love.png" alt="smiley with heart eyes"></img>
+//         <img class= "badges" src="/Assets/Images/Badges/old-man.png" alt="old man"></img>
+//         <img class= "badges" src="/Assets/Images/Badges/question.jpg" alt="question mark"></img>
+//         <img class= "badges" src="/Assets/Images/Badges/quoteBubbles.jpg" alt="quote bubbles"></img>
+//         <img class= "badges" src="/Assets/Images/Badges/speech.png" alt="person giving speech"></img>
+//         <img class= "badges" src="/Assets/Images/Badges/spoon-and-fork.png" alt="spoon and fork"></img>
+//         <img class= "badges" src="/Assets/Images/Badges/thumbs-down.png" alt="thumbs down"></img>
+//         <img class= "badges" src="/Assets/Images/Badges/smiley1.jpg" alt="smiley face"></img>
+//         <img class= "badges" src="/Assets/Images/Badges/romantic-date.png" alt="people in love"></img>
