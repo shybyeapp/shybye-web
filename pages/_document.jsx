2aa1140c4eb86896b010e7b5f9f125a11301d8b0
J. Lewis <inquiries.lewis+dev@gmail.com>
2022-12-09 22:38:59 -0500

    Static assets and compiled output (Following migration guide on the Next.js documentation: https://nextjs.org/docs/migrating/from-create-react-app\#static-assets-and-compiled-output) [x] Moved all images, fonts, and other static assets to the 'public' directory (legacy font downloads added via the Google Fonts CSS v2 API (https://developers.google.com/fonts/docs/css2) on 'pages/_document.js'; may reduce bundle size) [x] Converted CRA entry points ('src/App.js', 'src/index.js', and 'public/index.html') to Next.js ('pages/_app.jsx' and 'pages/_document.jsx' respectively) [x] Imported stylesheets where necessary (also added a 'jsconfig.json' with paths configuration to make importing more intuitive) [x] Added '.next' directory to '.gitignore' Additionally: - renamed 'public' subdirectories to match project convention (lowercase) - deleted unnecessary 'public/_redirects' file - renamed 'src/Images/Logos' to 'public/images/brand'

---
+++ pages/_document.jsx
@@ -0,0 +1,27 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+import "@/index.css"
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <meta charset="utf-8" />
+        <meta name="theme-color" content="#000000" />
+        <meta name="description" content="Complete challenges, collect rewards, reduce social anxiety!" />
+
+        {/* Google Fonts (variable): "Inter", "Josefin Sans" */}
+        {/** @todo ensure compatibility with older browsers */}
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter&family=Josefin+Sans" />
+
+        {/** @todo create app manifest and icons via RealFaviconGenerator.net (when final app branding is decided) */}
+        <link rel="icon" id="favicon" href="images/favicon.ico" />
+        <link rel="manifest" href="manifest.json" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
