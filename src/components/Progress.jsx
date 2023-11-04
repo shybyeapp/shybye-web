cc05ca66226f36973383e3352e6b6d30c0deade2
Chee <60206746+cheemurakami@users.noreply.github.com>
2023-11-04 23:20:19 +0000

    update base layout

--- src/components/Progress.jsx
+++ src/components/Progress.jsx
@@ -14,23 +14,34 @@ const Progress = () => {
       }}
     >
       <ProgressContainer>
-        <BackgroundBox>
-          background image
-        </BackgroundBox>
-        <ProgressBarBox>
-          <ProfilePicBox>
-            <img
-              src='../../../images/badges/speech.png'
-              width={100}
-              height={100}
-            />
-            <EditLink>Edit Profile</EditLink>
-            <ProgressBar>Progress Bar</ProgressBar>
-          </ProfilePicBox>
-        </ProgressBarBox>
-        <OtherProgressBox>
-          other progress
-        </OtherProgressBox>
+        <ProgressBox>
+          <ProfileBox>
+            <ProfilePicBox>
+              <img
+                src='../../../images/badges/speech.png'
+                width={100}
+                height={100}
+              />
+              <EditLink>Edit Profile</EditLink>
+            </ProfilePicBox>
+
+            <ProgressBar>
+              <LevelBox>
+                <LevelText>Level</LevelText>
+                <Level>0</Level>
+              </LevelBox>
+              <Bar>
+                <div>Bar</div>
+              </Bar>
+              <PointsBox>
+                <Points>0/5 pts</Points>
+              </PointsBox>
+            </ProgressBar>
+          </ProfileBox>
+          <StatusContainer>
+            Status Container
+          </StatusContainer>
+        </ProgressBox>
       </ProgressContainer>
     </Grid>
   )
@@ -43,27 +54,33 @@ const ProgressContainer = styled.div`
   justify-content: center;
 `;
 
-const BackgroundBox = styled.div`
+const ProgressBox = styled.div`
+  align-items: center;
+  border-radius: 5px;
+  display: flex;
+  flex-direction: column;
+  height: 480px;
+  justify-content: center;
+  width: 400px;
+`;
+
+const ProfileBox = styled.div`
+  align-items: center;
   border: 1px solid #DFDFDF;
   border-top-left-radius: 5px;
   border-top-right-radius: 5px;
-  height: 150px;
-  width: 420px;
-`;
-
-const ProgressBarBox = styled.div`
-  border: 1px solid #DFDFDF;
   display: flex;
-  height: 210px;
-  justify-content: center;
-  width: 420px;
+  flex-direction: column;
+  height: 300px;
+  justify-content: space-around;
+  width: 400px;
 `;
 
 const ProfilePicBox = styled.div`
   background-color: white;
+  border: 1px solid #DFDFDF;
   border-radius: 50%;
   height: 100px;
-  margin: -50px 50px 0;
   width: 100px;
 `;
 
@@ -71,14 +88,60 @@ const EditLink = styled.p`
   text-decoration: underline
 `;
 
-const ProgressBar = styled.div``;
+const ProgressBar = styled.div`
+  align-items: center;
+  display: flex;
+  height: 80px;
+  justify-content: space-around;
+  width: 380px;
+`;
 
-const OtherProgressBox = styled.div`
+const LevelBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 60px;
+  width: 50px;
+`;
+
+const LevelText = styled.div`
+  font-size: 16px;
+  margin: auto;
+`;
+
+const Level = styled.div`
+  font-size: 36px;
+`;
+
+const Bar = styled.div`
+  align-items: center;
+  border: 1px solid #DFDFDF;
+  display: flex;
+  height: 60px;
+  justify-content: center;
+  width: 200px;
+`;
+
+const PointsBox = styled.div`
+  height: 60px;
+  width: 80px;
+`;
+
+const Points = styled.p`
+  line-height: 24px;
+  text-align: center;
+`;
+
+const StatusContainer = styled.div`
+  align-items: center;
   border: 1px solid #DFDFDF;
   border-bottom-left-radius: 5px;
   border-bottom-right-radius: 5px;
-  height: 150px;
-  width: 420px;
+  border-top: none;
+  display: flex;
+  flex-direction: column;
+  height: 120px;
+  justify-content: space-around;
+  width: 400px;
 `;
 
 export default Progress
